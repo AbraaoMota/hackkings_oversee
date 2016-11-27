2fed395217a1a93f7f208cc037baf32770948e9c
diogofernandesc <diogofernandescon@gmail.com>
2016-11-27 04:46:46 +0000

    Testing event handling, not working

--- django/oversee/schedule/views.py
+++ django/oversee/schedule/views.py
@@ -7,10 +7,25 @@ def index(request):
  # return HttpResponse("Hello, world. You can make a schedule here")
 
 def home(request):
+	if(request.GET.get('mybtn')):
+		return redirect('dashboard')
 	return render(request, "schedule/home.html")
 
 def dashboard(request):
 	return render(request, "schedule/dashboard.html")
+
+def new_channel(request):
+	return render(request, "schedule/new_channel.html")
+
+def user_add_availability(request):
+	return render(request, "schedule/")
+
+
+
+# Testing:
+
+
+
 
 
 
